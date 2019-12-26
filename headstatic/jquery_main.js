84e2ac1173d7533cb18986c88cfb5c2d8bd49ef6
ki <ichi9k16@gmail.com>
2019-12-26 13:52:34 +0900

    css

---
+++ headstatic/jquery_main.js
@@ -0,0 +1,10 @@
+    $(function(){
+        var countId = 1
+        $("#content h1,#content h2,#content h3,#content h4,#content h5,#content h6").each(function(){
+            var ttl = $(this).text();
+            var lv = this.nodeName.toLowerCase();
+            this.id = 'ttl-' + countId;
+            countId ++;
+            $("#index").append('<dd class="lv_'+lv+'"><a href="#'+this.id+'">'+ttl+'</a></dd>');
+        });
+    });
